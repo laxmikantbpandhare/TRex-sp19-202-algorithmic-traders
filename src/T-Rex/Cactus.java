c80279869d17332adcdadcee8b05800fb399e8c9
wamiquem <wamique.ansari@sjsu.edu>
2019-04-11 01:47:55 -0700

    Adding prototype pattern to clone objects

--- src/T-Rex/Cactus.java
+++ src/T-Rex/Cactus.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Cactus extends Actor
+public class Cactus extends Actor implements Cloneable
 {
     /**
      * Act - do whatever the Cactus wants to do. This method is called whenever
@@ -24,4 +24,9 @@ public class Cactus extends Actor
             setLocation(599,y);
         }
     }    
+    
+    @Override 
+    public Cactus clone() throws CloneNotSupportedException {
+          return (Cactus) super.clone();
+    }
 }
--- src/T-Rex/Stones.java
+++ src/T-Rex/Stones.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Stones extends Actor
+public class Stones extends Actor implements Cloneable
 {
     /**
      * Act - do whatever the Stones wants to do. This method is called whenever
@@ -23,5 +23,10 @@ public class Stones extends Actor
         if (x==0) {
             setLocation(599,y);
         }
-    }    
+    }  
+    
+    @Override 
+    public Stones clone() throws CloneNotSupportedException {
+          return (Stones) super.clone();
+    }
 }
