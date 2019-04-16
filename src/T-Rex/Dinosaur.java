e29765617032b915b0b9017d9f33650d8c5b0db4
wamiquem <wamique.ansari@sjsu.edu>
2019-04-16 00:11:16 -0700

    Resolved merge conflicts

--- src/T-Rex/Dinosaur.java
+++ src/T-Rex/Dinosaur.java
@@ -1,5 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.List;
+import java.util.ArrayList;
 /**
  * Write a description of class TRex here.
  * 
@@ -17,7 +18,7 @@ public class Dinosaur extends Leaf
         getImage().scale(getImage().getWidth()*40/100, getImage().getHeight()*40/100);
         velocity=0;
     }
-    
+
     /**
      * Act - do whatever the TRex wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -33,8 +34,15 @@ public class Dinosaur extends Leaf
         }
         fall();
         if (Greenfoot.isKeyDown("space") && getY() > 296) jump();
+
+        if(touch(Coin.class) || touch(Food.class)) {
+            Actor touched = getOneIntersectingObject(Actor.class);
+            System.out.println(touched.getClass().getName());
+            getWorld().removeObject(touched);
+            //Greenfoot.stop();
+        }
     }
-    
+
     public void fall()
     {
         setLocation(getX(), getY() + velocity);
@@ -43,15 +51,66 @@ public class Dinosaur extends Leaf
         else
             velocity = velocity + GRAVITY;
     }
-    
+
     public void jump()
     {
         velocity = velocity - 20;
     }
-    
+
     public void checkKeys()
     {
         if(Greenfoot.isKeyDown("space") && getY() > 296)
             jump();
+    }  
+
+    /** This method is a pixel perfect collision detection. 
+     * Return, if it intersects an actor of the given class */
+    public boolean touch(Class c)
+    {
+        List<Actor> list =
+            getWorld().getObjects(c),
+        list2 = new ArrayList();
+        for(Actor A : list)
+            if(A!=this && intersects(A) && touch(A))
+                return true;
+        return false;
+    }  
+
+    /** This method is a pixel perfect collision detection. 
+     * Returns true, if the object touchs the given Actor */
+    public boolean touch(Actor a_big)
+    {
+        Actor a_small;
+        if(getImage().getWidth()*getImage().getHeight()>a_big.getImage().getHeight()*a_big.getImage().getWidth())
+        {
+            a_small=a_big;
+            a_big=this;
+        }
+        else
+            a_small=this;
+
+        int i_hypot=(int)Math.hypot(a_small.getImage().getWidth(),a_small.getImage().getHeight());
+
+        GreenfootImage i=new GreenfootImage(i_hypot,i_hypot);
+        i.drawImage(a_small.getImage(),i_hypot/2-a_small.getImage().getWidth()/2,i_hypot/2-a_small.getImage().getHeight()/2);
+        i.rotate(a_small.getRotation());
+        int w=i_hypot;
+
+        GreenfootImage Ai = a_big.getImage(),
+        i2=new GreenfootImage(i_hypot=(int)Math.hypot(Ai.getWidth(),Ai.getHeight()),i_hypot);
+        i2.drawImage(Ai,i2.getWidth()/2-Ai.getWidth()/2,i2.getHeight()/2-Ai.getHeight()/2);
+        i2.rotate(a_big.getRotation());
+        Ai=i2;
+
+        int
+        x_Offset=a_big.getX()-a_small.getX()-(Ai.getWidth()/2-w/2),
+        y_Offset=a_big.getY()-a_small.getY()-(Ai.getHeight()/2-w/2);
+
+        boolean b = true;
+        for(int yi =Math.max(0,y_Offset); yi<w && yi<i_hypot+y_Offset && b; yi++)
+            for(int xi =Math.max(0,x_Offset); xi<w && xi<i_hypot+x_Offset && b; xi++)
+                if(Ai.getColorAt(xi-x_Offset,yi-y_Offset).getAlpha()>0 && i.getColorAt(xi,yi).getAlpha()>0)
+                    b=false;
+        return !b;
     }
 }
