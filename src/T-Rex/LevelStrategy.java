0b392c1e1dd1a11e47e3b2abfc3bde0fe2500344
Suyash906 <suyash90682711@gmail.com>
2019-04-19 10:10:00 -0700

    level strategy implemented

---
+++ src/T-Rex/LevelStrategy.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;
+/**
+ * Write a description of class LevelStrategy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LevelStrategy extends Actor implements ILevelStrategy
+{
+    /**
+     * Act - do whatever the LevelStrategy wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }
+    
+    public void gameDisplay(){
+        
+    };
+    public void addLandObstacles(){};
+    public void addSkyObstacles(){};
+    public void addRewards(){};
+    public void setObstacleSpeed(){};
+    public void setRewardsSpeed(){};
+    public void setRewardsScoreValue(){};
+}
